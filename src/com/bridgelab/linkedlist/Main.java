7d4594d03d2e091135319aa8aecafc9a659ec72c
mihirpatil0 <mihirrpatil@gmail.com>
2021-07-03 03:21:27 +0530

    [mihir] Modified : added new node with data 40 between node 30 and 70.

--- src/com/bridgelab/linkedlist/Main.java
+++ src/com/bridgelab/linkedlist/Main.java
@@ -69,6 +69,16 @@ public class Main
 		linkedlistSearchElement.appendAtLast(70);
 		linkedlistSearchElement.printNode();
 		System.out.println("\n"+ "Searching for specific element in linkedlist :" + "\n");
-		linkedlistSearchElement.searchElement(30);//providing index number.
+		linkedlistSearchElement.searchElement(30);//providing data 
+		
+		System.out.println("-----------------------------------------------------");
+		
+		System.out.println("\n"+ "Adding new node 40 after 30 :" + "\n");
+		System.out.println("Linkedlist before adding new node in between.");
+		linkedlistNodeInsertAt.printNode();
+		System.out.println("LinkedList after adding new node 40 between 30 and 70");
+		//adding new node between 56 and 70.
+		linkedlistNodeInsertAt.insertAtLocation(2, 40);
+		linkedlistNodeInsertAt.printNode();
 	}
 }
